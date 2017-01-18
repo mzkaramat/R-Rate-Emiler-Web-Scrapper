8724f618d2f998a3985b043aacb0dec8a0bb3ac6
Xeaphii <muhammadzeeshan020@gmail.com>
2017-01-18 14:14:26 +0500

    first commit

---
+++ index.R
@@ -0,0 +1,27 @@
+library(XML)
+library(mailR)
+
+while ( 1 == 1){
+  
+
+
+theurl <- "http://paragonexchange.pk/"
+tables <- readHTMLTable(theurl)
+n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
+
+out <- tables[[which.max(n.rows)]]
+as.numeric(out[out$V1 == 'EUR',]$V3)
+rate <-  as.numeric(as.character(out[out$V1 == 'EUR',]$V3))[1]
+rate
+
+
+send.mail(from = "muhammadzeeshan020@gmail.com",
+          to = c("zeeshan.karamat@techlogix.com"),
+          
+          subject = paste("Rate is",rate),
+          body = "Rate Reminder",
+          smtp = list(host.name = "119.30.86.103", port = 25),
+          authenticate = FALSE,
+          send = TRUE)
+Sys.sleep(60)
+}
